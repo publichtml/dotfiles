53b052a3275d1c5f3b0d6f163a7a1bf03e88bda0
publichtml <publi9html@gmail.com>
2018-11-27 10:19:58 +0900

    [zsh] Make independent runcoms directory

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -4,12 +4,13 @@ git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$H
 
 setopt EXTENDED_GLOB
 
+# 独自runcomsにsymbolic link
 script_dir=$(cd $(dirname $0) && pwd)
-for rcfile in "$script_dir"/^setup.sh(.N); do
+for rcfile in "$script_dir"/runcoms/*; do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
-# 上で既に作られたものは上書きされない
+# zpreztoのruncomsにsymbolic link (上で既に作られたものは上書きされない)
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
