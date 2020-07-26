d94970e4480b8cdfd1fb949dd16bf37545b394e8
wakana.miura <publi9html@gmail.com>
2020-07-26 18:25:59 +0900

    [vim] Install dein.vim on vim start

--- vim/setup.sh
+++ vim/setup.sh
@@ -10,3 +10,8 @@ done
 # .vimrc.local に symbolic link
 cp -n "$script_dir"/local/vimrc.local{.sample,}
 ln -s "$script_dir"/local/vimrc.local "$HOME/."vimrc.local
+
+# ~/.vim/dein.toml に symbolic link
+cp -n "$script_dir"/local/dein.toml.local{.sample,}
+mkdir -p "$HOME/".vim
+ln -s "$script_dir"/local/dein.toml.local "$HOME/".vim/dein.toml
